aadba772102c71c85107cfa5aa525a2c532f958b
joonwon <114joonwon@naver.com>
2026-04-01 19:11:45 +0900

    아이디 찾기, 비밀번호 찾기 화면 구현
    아이디,비밀번호 찾기 화면 구현

--- frontend/App.js
+++ frontend/App.js
@@ -5,6 +5,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import StartScreen from './src/screens/StartScreen/StartScreen'
 import LoginScreen from './src/screens/LoginScreen/LoginScreen'
 import SignUpScreen from './src/screens/SignUpScreen/SignUpScreen'
+import FindIdScreen from './src/screens/FindIdScreen/FindIdScreen'
+import FindPwScreen from './src/screens/FindPwScreen/FindPwScreen'
+import NewPwScreen from './src/screens/FindPwScreen/NewPwScreen'
 
 const Stack = createNativeStackNavigator()
 
@@ -25,6 +28,9 @@ export default function App() {
         <Stack.Screen name="Start" component={StartScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="FindId" component={FindIdScreen} />
+        <Stack.Screen name="FindPw" component={FindPwScreen} />
+        <Stack.Screen name="NewPw" component={NewPwScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
