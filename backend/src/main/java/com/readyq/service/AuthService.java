b70b3f0b35a3343e54ff7b4ee770880e07494eb5
Geonu999 <yk010410@naver.com>
2026-04-06 01:18:55 +0900

    소셜로그인

--- backend/src/main/java/com/readyq/service/AuthService.java
+++ backend/src/main/java/com/readyq/service/AuthService.java
@@ -44,6 +44,7 @@ public class AuthService {
         user.setNickname(req.getNickname());
         user.setEmail(req.getEmail());
         user.setPhone(req.getPhone());
+        user.setProvider("local");
 
         userRepository.save(user);
     }
